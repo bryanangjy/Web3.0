797d35b1dfaf69fbd1687e1e59b8ce63221a2e0c
bryanangjy <137162406+bryanangjy@users.noreply.github.com>
2024-09-17 21:51:26 +0800

    hardhat.config.js solidity changed from .17 to .0

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
   networks: {
     polygonzkevm: {
       url: 'https://polygonzkevm-cardona.g.alchemy.com/v2/Yc7IP8a_JBNItO1C-Sfsj5Q6OQcxDQJ2', // Replace with your Alchemy testnet API key
