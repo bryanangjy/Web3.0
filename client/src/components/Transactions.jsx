ed5aef0f114b61b878f95b4f806379bda8b0cc9f
bryanangjy <bryanangjy@gmail.com>
2024-09-18 06:38:46 +0800

    removed keyword

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -6,9 +6,8 @@ import dummyData from '../utils/dummyData';
 import { shortenAddress } from '../utils/shortenAddress';
 import useFetch from '../hooks/useFetch';
 
-const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
-    const { data: gifUrl } = useFetch(keyword); // Pass keyword as a string
-   
+const TransactionCard = ({ addressTo, addressFrom, timestamp, message, amount, url }) => {
+    // No need to use keyword here
     return (
         <div className="bg-[#181918] m-4 flex flex-1
             2x1:min-w-[450px]
@@ -33,15 +32,14 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
                         </>
                     )}
                 </div>
-                    <img 
-                        src={gifUrl || url}
-                        alt="gif"
-                        className="w-full h-64 2x:h-96 rounded-md shadow-lg object-cover"
-                    />
-
-                    <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-                        <p className="text-[#37c7da] font-bold">{timestamp}</p>
-                    </div>
+                <img 
+                    src={url} // No gifUrl since keyword is removed
+                    alt="gif"
+                    className="w-full h-64 2x:h-96 rounded-md shadow-lg object-cover"
+                />
+                <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+                    <p className="text-[#37c7da] font-bold">{timestamp}</p>
+                </div>
             </div>
         </div>
     )
@@ -58,10 +56,17 @@ const Transactions = () => {
                 ) : (
                     <h3 className="text-white text-3xl text-center my-2">Connect your account to see the latest transactions</h3>
                 )}
-
                 <div className="flex flex-wrap justify-center items-center mt-10">
                     {dummyData.reverse().map((transaction, i) => (
-                        <TransactionCard key={i} {...transaction} />
+                        <TransactionCard 
+                            key={i} 
+                            addressTo={transaction.addressTo}
+                            addressFrom={transaction.addressFrom}
+                            timestamp={transaction.timestamp}
+                            message={transaction.message}
+                            amount={transaction.amount}
+                            url={transaction.url}
+                        />
                     ))}
                 </div>
             </div>
